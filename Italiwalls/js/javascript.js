bc8732848b0f7faf279cebbd9c1df20795f515d1
Oscar Shue <oscarshue@hotmail.co.uk>
2021-04-06 13:55:37 +0100

    Added mobile optimization for Italiwalls

---
+++ Italiwalls/js/javascript.js
@@ -0,0 +1,9 @@
+/* Function to Open Navigation Menu */
+function openNav() {
+    var x = document.getElementById("nav_bar");
+    if (x.className === "nav_bar") {
+      x.className += " responsive";
+    } else {
+      x.className = "nav_bar";
+    }
+  }
